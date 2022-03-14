84c051d0e51338ba7de5e022323620b746797ead
GunelMustafayeva21 <100667536+GunelMustafayeva21@users.noreply.github.com>
2022-03-14 19:46:14 +0300

    Add files via upload

---
+++ ThreeTasks/ThreeTasks/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //Console.WriteLine("Negativ  ededler palindrom olmurlar");
+            //Console.WriteLine("0 ve ya musbet ededi daxil edin:");
+            //int eded = Convert.ToInt32(Console.ReadLine());
+
+            //if (eded <= 9) { Console.WriteLine("Palindrome"); return; }
+            //int qaliq = 0, tersi = 0, ededKopya = eded;
+            //while (eded > 0)
+            //{
+            //    qaliq = eded % 10;
+            //    tersi = tersi * 10 + qaliq;
+            //    eded /= 10;
+            //}
+            //if (ededKopya == tersi) { Console.WriteLine("Palindrome"); }
+            //else { Console.WriteLine("Palindrome deyil"); }
+            #endregion
+
+
+
+            #region
+            //Console.WriteLine("Enter number: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //if (n == 0)
+            //{
+            //    Console.WriteLine("1 ");
+            //}
+            //int count = 0;
+            //while (n != 0)
+            //{
+            //    n /= 10;
+            //    count++;
+            //}
+            //Console.WriteLine("Number of digits: ");
+            //Console.WriteLine(count);
+            #endregion
+
+
+            #region 
+            //Console.WriteLine("0 and 1 are not prime or composite");
+            //Console.WriteLine("Enter positive number: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 2; i * i < n; i++)
+            //{
+            //    if (n % i == 0)
+            //    {
+            //        Console.WriteLine("Number is not prime");
+            //        return;
+            //    }
+            //}
+            //Console.WriteLine("Number is Prime");
+            #endregion
+
+        }
+    }
+}
