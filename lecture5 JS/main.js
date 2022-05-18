e970e2796bac4e989c2ff443f2a85021eff8310a
GunelMustafayeva21 <100667536+GunelMustafayeva21@users.noreply.github.com>
2022-05-18 13:21:15 +0400

    Fifth JS Project Responsive Template creation

---
+++ lecture5 JS/main.js
@@ -0,0 +1,12 @@
+//draggable="true"; //inside div
+//choose div as draggable
+//draggable.ondragstart=function(){
+    //console.log("start to drag");
+    //draggable.style.border
+//}
+//ondragenter ondragleave ondragover borderi sil ondrop prevenDefault  droppable.append(draggable) hamsi ucucn selector all
+//hamsine ferqli id ver
+//for ileg et attribute istifade ederek id-leri menimset appende id ver
+//align-content:flex-start
+//image splitter
+
